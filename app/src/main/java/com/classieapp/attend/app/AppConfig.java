9a10fa43846a02dba4628ad8180d82c327f1e03a
Glen Despaux Jr <gdespaux@nolahousecall.com>
2017-03-22 17:16:40 -0500

    Initial Commit

---
+++ app/src/main/java/com/classieapp/attend/app/AppConfig.java
@@ -0,0 +1,9 @@
+package com.classieapp.attend.app;
+
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://classie.nhctechservices.com/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://classie.nhctechservices.com/register.php";
+}
