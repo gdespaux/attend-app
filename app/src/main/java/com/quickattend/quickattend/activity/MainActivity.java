57118350fd83a5030102829f2e70ed0e81b6b013
Glen Despaux Jr <gdespaux@nolahousecall.com>
2017-04-18 17:05:05 -0500

    Added placeholder for first run intro

--- app/src/main/java/com/quickattend/quickattend/activity/MainActivity.java
+++ app/src/main/java/com/quickattend/quickattend/activity/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -154,6 +155,39 @@ public class MainActivity extends AppCompatActivity
                 .setInvocationEvent(InstabugInvocationEvent.NONE)
                 .build();
         Instabug.identifyUser(email, email);
+
+        //  Declare a new thread to do a preference check
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                //  Initialize SharedPreferences
+                SharedPreferences getPrefs = PreferenceManager
+                        .getDefaultSharedPreferences(getBaseContext());
+
+                //  Create a new boolean and preference and set it to true
+                boolean isFirstStart = getPrefs.getBoolean("firstStart", true);
+
+                //  If the activity has never started before...
+                if (isFirstStart) {
+
+                    //  Launch app intro
+                    Intent i = new Intent(MainActivity.this, IntroActivity.class);
+                    startActivity(i);
+
+                    //  Make a new preferences editor
+                    //SharedPreferences.Editor e = getPrefs.edit();
+
+                    //  Edit preference to make it false because we don't want this to run again
+                    //e.putBoolean("firstStart", false);
+
+                    //  Apply changes
+                    //e.apply();
+                }
+            }
+        });
+
+        // Start the thread
+        t.start();
     }
 
     // Fetches reg id from shared preferences
